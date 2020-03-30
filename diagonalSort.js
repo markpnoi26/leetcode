6d18fe6179716bd722778c555772eb6b5cb0b5ef
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-03-30 00:09:47 -0700

    working on diagonal sort in 2d matrix

---
+++ diagonalSort.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var diagonalSort = function(mat) {
+    for (let i =0; i<mat.length-1; i++) {
+        if (i=0) {
+            for (let j=0; j<mat[i].length-1; j++) {
+           
+            }
+        } else {
+            for (let j=0; j<mat[])
+        }
+        
+    }
+};
+
+let mat = [[3,3,1,1],[2,2,1,2],[1,1,1,2]]
+console.log(diagonalSort(mat), [[1,1,1,1],[1,2,2,2],[1,2,3,3]])

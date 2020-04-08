00b704fba0f69df85798d87c70d90e2ffdaca8ba
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-08 10:27:43 -0700

    added DP solution to problem

--- coinChange.js
+++ coinChange.js
@@ -34,23 +34,40 @@ const coinChange = (coins, amount) => {
     return(min === Infinity? -1:min)
 }
 
+const coinChangeDP = (coins, amount) => {
+   
+    // because Array are zero indexed, we need to add another one to have dp[amount]
+    const dp = new Array(amount + 1).fill(Infinity)
+    
+    dp[0] = 0
+
+    for  (let i=1; i<=amount; i++) {
+        for (let coin of coins) {
+            if (i >= coin) {
+                dp[i] = Math.min(dp[i], dp[i-coin]+1)
+            }
+        }
+    }
+    return dp[amount] === Infinity? -1:dp[amount]
+
+}
 
 
 
 let coins = [1, 2, 5], amount = 11
-console.log(coinChange(coins, amount), 3)
+console.log(coinChangeDP(coins, amount), 3)
 
 coins = [2], amount = 3
-console.log(coinChange(coins, amount), -1)
+console.log(coinChangeDP(coins, amount), -1)
 
 coins = [1], amount = 0
-console.log(coinChange(coins, amount), 0)
+console.log(coinChangeDP(coins, amount), 0)
 
 coins = [227, 99, 328, 299, 42, 322], amount = 9847 
-console.log(coinChange(coins, amount), 31)
+console.log(coinChangeDP(coins, amount), 31)
 
 coins = [288, 160, 10, 249, 40, 77, 314, 429], amount = 9208
-console.log(coinChange(coins, amount), 22)
+console.log(coinChangeDP(coins, amount), 22)
 
 coins = [338, 26, 303, 41, 167, 331, 485, 239, 332], amount = 8966
-console.log(coinChange(coins, amount), 20)
+console.log(coinChangeDP(coins, amount), 20)
